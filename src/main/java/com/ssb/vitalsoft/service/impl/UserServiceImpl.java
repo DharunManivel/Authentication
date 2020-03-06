474f16cea23dc18ef95e19395be4d4d6efa7830a
Krishnakumar K <krishnakumar548@gmail.com>
2020-03-06 18:50:15 +0530

    JWT integrated

--- src/main/java/com/ssb/vitalsoft/service/impl/UserServiceImpl.java
+++ src/main/java/com/ssb/vitalsoft/service/impl/UserServiceImpl.java
@@ -1,15 +1,20 @@
 package com.ssb.vitalsoft.service.impl;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.ssb.vitalsoft.dao.UserRepository;
 import com.ssb.vitalsoft.model.User;
 import com.ssb.vitalsoft.service.UserService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 
 @Service("userService")
 public class UserServiceImpl implements UserService {
@@ -22,10 +27,18 @@ public class UserServiceImpl implements UserService {
     public String login(String username, String password) {
         Optional<User> user = userRepository.login(username,password);
         if(user.isPresent()){
-            String token = UUID.randomUUID().toString();
+
+
+
+            String token = JWT.create()
+          //          .withSubject( ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername())
+                    .withExpiresAt(new Date(System.currentTimeMillis() + 30000))
+                    .sign(HMAC512("SecretKeyToGenJWTs".getBytes()));
             User user1= user.get();
             user1.setToken(token);
             userRepository.save(user1);
+
+
             return token;
         }
 
@@ -38,6 +51,10 @@ public class UserServiceImpl implements UserService {
         Optional<User> user= userRepository.findByToken(token);
         if(user.isPresent()){
             User user1 = user.get();
+
+                String.valueOf(JWT.require(Algorithm.HMAC512("SecretKeyToGenJWTs".getBytes()))
+                        .build()
+                        .verify(token));
 
             return Optional.of(user1);
         }
